3df026debc6362f60e2f2c62706cb0bab3c94ccd
Rincewind60 <paul@thescobells.com>
2024-12-15 11:49:15 +1100

    Fix - Toggle Modifiers menu button depending on wether a game instance is selected or not. Fix - Crash on "New".

--- ViewModels/FilterToolbarVM.cs
+++ ViewModels/FilterToolbarVM.cs
@@ -44,13 +44,26 @@ public partial class FilterToolbarVM : ObservableRecipient
 
     public void ResetFilters(RinceDCSInstance currentInstance)
     {
-        Aircraft = ["All", .. (from ac in currentInstance.Aircraft select ac.Name).Order()];
-        Categories = ["All", .. (from grp in currentInstance.Groups.Groups select grp.Category).Distinct().Select(x => String.IsNullOrWhiteSpace(x) ? "None" : x).Order()];
-        Groups = ["All", .. (from grp in currentInstance.Groups.Groups select grp.Name).Order()];
+        if (currentInstance == null)
+        {
+            Aircraft = [];
+            Categories = [];
+            Groups = [];
 
-        SelectedAircraft = Aircraft.Where(a => a == Settings.Default.SelectedAircraft).FirstOrDefault("All");
-        SelectedCategory = Categories.Where(a => a == Settings.Default.SelectedCategory).FirstOrDefault("All");
-        SelectedGroup = Groups.Where(a => a == Settings.Default.SelectedGroup).FirstOrDefault("All");
+            SelectedAircraft = null;
+            SelectedCategory = null;
+            SelectedGroup = null;
+        }
+        else
+        {
+            Aircraft = ["All", .. (from ac in currentInstance.Aircraft select ac.Name).Order()];
+            Categories = ["All", .. (from grp in currentInstance.Groups.Groups select grp.Category).Distinct().Select(x => String.IsNullOrWhiteSpace(x) ? "None" : x).Order()];
+            Groups = ["All", .. (from grp in currentInstance.Groups.Groups select grp.Name).Order()];
+
+            SelectedAircraft = Aircraft.Where(a => a == Settings.Default.SelectedAircraft).FirstOrDefault("All");
+            SelectedCategory = Categories.Where(a => a == Settings.Default.SelectedCategory).FirstOrDefault("All");
+            SelectedGroup = Groups.Where(a => a == Settings.Default.SelectedGroup).FirstOrDefault("All");
+        }
         WithButtons = Settings.Default.WithButtons;
     }
 
